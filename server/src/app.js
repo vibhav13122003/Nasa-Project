883df83b67e1ea1a9c4113035edd31c3ba697c1e
vibhav13122003 <vibhavbhartiya13@gmail.com>
2024-02-21 19:30:05 +0530

    scripts added

--- server/src/app.js
+++ server/src/app.js
@@ -7,19 +7,28 @@ const planetsRouter = require("./routes/planets/planets.router");
 const launchesRouter = require("./routes/launches/launches.router");
 const app = express();
 
+// Enable CORS for development
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+
+// Enable request logging
 app.use(morgan("combined"));
 
+// Parse JSON requests
 app.use(express.json());
+
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+// API routes
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
-app.get("/*", (req, res) => {
+
+// Catch-all route to serve the client's HTML file
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
